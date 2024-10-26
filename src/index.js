6874f07e9c35bb74d22db51b05e01986098fa142
WisniaFresh <kuba.webdev@gmail.com>
2024-10-26 13:13:57 +0200

    api prefix

--- src/index.js
+++ src/index.js
@@ -19,13 +19,13 @@ app.get("/", async (req, res, next) => {
   });
 });
 
-app.get("/path", (req, res, next) => {
+app.get("/api/path", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from path!",
   });
 });
 
-app.get("/leads/:id", async (req, res, next) => {
+app.get("/api/leads/:id", async (req, res, next) => {
   const { id } = req.params;
   const results = await crud.getLead(id);
   return res.status(200).json({
@@ -35,7 +35,7 @@ app.get("/leads/:id", async (req, res, next) => {
   });
 });
 
-app.get("/leads", async (req, res, next) => {
+app.get("/api/leads", async (req, res, next) => {
   const results = await crud.listLeads();
   return res.status(200).json({
     message: "Hello from get leads path!",
@@ -43,7 +43,7 @@ app.get("/leads", async (req, res, next) => {
   });
 });
 
-app.post("/leads", async (req, res, next) => {
+app.post("/api/leads", async (req, res, next) => {
   const postData = await req.body;
   console.log("postData", postData);
 
